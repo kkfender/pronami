c917e1219af540473757dafebebb0a3510bb26d8
root <root@localhost.localdomain>
2022-04-07 20:18:18 +0900

    参照を編集

--- product/pro_disp.php
+++ product/pro_disp.php
@@ -13,16 +13,30 @@ $password = 'root';
 $dbh = new PDO($dsn, $user, $password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = 'SELECT name FROM mst_product WHERE code = ?';
+$sql = 'SELECT name,price,gazou FROM mst_product WHERE code = ?';
 $stmt = $dbh->prepare($sql);
 $data[] = $pro_code;
 $stmt->execute($data);
 
 $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 $pro_name = $rec['name'];
+$pro_price = $rec['price'];
+$pro_gazou_name = $rec['gazou'];
+
 
 $dbh = null;
 
+if ($pro_gazou_name=='')
+{
+	$disp_gazou='';
+}
+else
+{
+	$disp_gazou='<img src="./gazou/'.$pro_gazou_name.'">';
+}
+
+
+var_dump(disp_gazou);
 }
 catch (Exception $e)
 {
@@ -41,6 +55,8 @@ catch (Exception $e)
 <?php print $pro_name; ?>
 <br/>
 <br/>
+<?php print $disp_gazou; ?>
+<br />
 <form>
 <input type="button" onclick="history.back()" value="戻る"><br/>
 </form>
